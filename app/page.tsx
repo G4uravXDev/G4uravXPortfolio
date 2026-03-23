f4fc07846134fe99e2fec42b096c4259d9be60ef
Gaurav Nayyar <143985316+G4uravXDev@users.noreply.github.com>
2026-03-23 08:05:02 +0530

    Age of ultron

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             <AboutSection />
 
             {/* Experience */}
-            <CareerSection />
+            {/* <CareerSection /> */}
 
             {/* Projects */}
             <ProjectsSection />
